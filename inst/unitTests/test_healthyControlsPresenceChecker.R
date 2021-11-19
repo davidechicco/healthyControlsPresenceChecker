3c1ca598a73f0b4cb1719da8fc2ac07a9f982f8b
davidechicco <davide.chicco@gmail.com>
2021-11-19 09:42:35 -0500

    committed changes locally

---
+++ inst/unitTests/test_healthyControlsPresenceChecker.R
@@ -0,0 +1,11 @@
+# test_healthyControlsCheck <- function() {
+test_healthyControlsCheck <- function() {
+    checkEquals(healthyControlsCheck("GSE19429", FALSE), TRUE)
+    checkEquals(healthyControlsCheck("GSE58831", FALSE), FALSE)
+    checkEquals(healthyControlsCheck("GSE19429", FALSE), TRUE)
+    checkEquals(healthyControlsCheck("GSE34111", FALSE), TRUE)
+    checkEquals(healthyControlsCheck("GSE47407", FALSE), FALSE)
+    checkEquals(healthyControlsCheck("GSE32123131231", FALSE), NULL)
+}
+
+
