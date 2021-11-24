75bf230a23d0e57cdeb8b33ac4f18bd57944bcee
davidechicco <davide.chicco@gmail.com>
2021-11-24 10:03:37 -0500

    committed changes locally

---
+++ R/healthyControPresenceChecker-package.r
@@ -0,0 +1,10 @@
+#' healthyControlsChecker: A package for verifying the presence of healthy controls in a GEO dataset
+#'
+#' The healthyControlsChecker package provides one relevant function: healthyControlsCheck()
+#' 
+#' @section healthyControlsCheck() function:  The healthyControlsCheck() function readsin the GEO code of a dataset, and returns true if there's at least a feature containing the healthy controls.
+#'
+#' @docType package
+#' @name healthyControlsChecker
+NULL
+#> NULL
